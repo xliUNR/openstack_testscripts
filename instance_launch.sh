0c40be6ba784a4e815fb504212a541ed9eb70277
Xiang Li <xli@nevada.unr.edu>
2020-09-16 00:32:11 +0000

    Added functionality to write data to file. Working on getting variables to work to be able to launch instnace

--- instance_launch.sh
+++ instance_launch.sh
@@ -4,16 +4,25 @@
 
 # Variables
 CRED="/root/demo-openrc"
-IMAGE=""
-FLAVOR=""
-SECGROUP=""
-TRIALS=4
+IMAGE="cirros"
+FLAVOR="m.nano"
+SECGROUP="default"
+KEYPAIR="demotestkey1"
+INSTANCE_NAME="time_test $i"
+TRIALS=1
+# Set output file name. Uses parameter expansion. 
+# If $1 (first command line arg) is not set/null, then set it to whatever is after :-
+OUTPUTFILE=${1:-unnamed_data.txt}
+OS_CMD="openstack server list" 
+OS_PARAMS="--flavor $FLAVOR --image $IMAGE --key-name $KEYPAIR --security-group $SECGROUP $INSTANCE_NAME"
+#OS_RUN=$OS_CMD $OS_PARAMS
 #First source the demo credentials
 source $CRED
-
+#$OS_CMD
 # Launch an instance
 for (( i=0; i < $TRIALS; i++ ))
 do
-    { echo "Trial #: " ; $i } >>test.txt
-    { time openstack image list ; } 2>> test.txt 
+    { printf "\nTrial #: $i" ; } >>$OUTPUTFILE
+    #{ time openstack image list ; } 2>> $OUTPUTFILE
+    { eval $OS_RUN ; } 2>> $OUTPUTFILE  
 done
