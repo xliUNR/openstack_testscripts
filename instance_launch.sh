cbdb2b4fdcb4e38bef4f3768f6657d2497c0f844
Xiang Li <xli@nevada.unr.edu>
2020-09-17 22:22:21 +0000

    Bash script works for multiple instance launches. Cleaned up code and added descriptions

--- instance_launch.sh
+++ instance_launch.sh
@@ -2,27 +2,37 @@
 
 #This scripts collect timing for instance launching using the openstack CLI.
 
-# Variables
+# Declare variables
 CRED="/root/demo-openrc"
 IMAGE="cirros"
-FLAVOR="m.nano"
+FLAVOR="m1.nano"
 SECGROUP="default"
 KEYPAIR="demotestkey1"
-INSTANCE_NAME="time_test $i"
-TRIALS=1
-# Set output file name. Uses parameter expansion. 
+INSTANCE_NAME="time_test"
+NETWORK="selfservice"
+
+# Number of trials you would like to run
+TRIALS=2
+
+#Set output file name. Uses parameter expansion. 
 # If $1 (first command line arg) is not set/null, then set it to whatever is after :-
 OUTPUTFILE=${1:-unnamed_data.txt}
-OS_CMD="openstack server list" 
-OS_PARAMS="--flavor $FLAVOR --image $IMAGE --key-name $KEYPAIR --security-group $SECGROUP $INSTANCE_NAME"
-#OS_RUN=$OS_CMD $OS_PARAMS
+
+# Set command and parameters you would like to test
+OS_CMD="openstack server create" 
+OS_PARAMS=" --flavor $FLAVOR --image $IMAGE --key-name $KEYPAIR --network $NETWORK --security-group $SECGROUP"
+
+# Combine command and parameters into one 
+OS_RUN=$OS_CMD$OS_PARAMS
+
 #First source the demo credentials
 source $CRED
-#$OS_CMD
+
 # Launch an instance
 for (( i=0; i < $TRIALS; i++ ))
 do
-    { printf "\nTrial #: $i" ; } >>$OUTPUTFILE
-    #{ time openstack image list ; } 2>> $OUTPUTFILE
-    { eval $OS_RUN ; } 2>> $OUTPUTFILE  
+     INSTANCE_NAME=" time_test$i"
+     { printf "\nTrial #: $i" ; } >>$OUTPUTFILE
+     { time $OS_RUN$INSTANCE_NAME; } 2>> $OUTPUTFILE 
+    
 done
