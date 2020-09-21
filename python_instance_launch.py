2f4d8c2a1a8a31e13ff6d631878c69420a58982a
Xiang Li <xli@nevada.unr.edu>
2020-09-21 23:12:21 +0000

    got credential verification and flavor list to work

--- python_instance_launch.py
+++ python_instance_launch.py
@@ -2,15 +2,19 @@
 import os
 from keystoneauth1 import loading
 from keystoneauth1 import session
+import novaclient
 from novaclient import client
-
+import keystoneclient.v2_0.client as ksclient
 
 def get_keystone_cred():
     d = {}
     d['username'] = os.environ['OS_USERNAME']
     d['password'] = os.environ['OS_PASSWORD']
     d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['tenant_name'] = os.environ['OS_TENANT_NAME']
+    d['project_name'] = os.environ['OS_PROJECT_NAME']
+    d['user_domain_name'] = os.environ['OS_USER_DOMAIN_NAME']
+    d['project_domain_name'] = os.environ['OS_PROJECT_DOMAIN_NAME']
+    #d['tenant_name'] = os.environ['OS_TENANT_NAME']
     return d
 
 def get_nova_cred():
@@ -19,4 +23,14 @@ def get_nova_cred():
     d['api-key'] = os.environ['OS_PASSWORD']
 print "Hello"
 
+creds = get_keystone_cred()
+#keystone = ksclient.Client(**creds)
 
+# Load credentials
+loader = loading.get_plugin_loader('password')
+auth = loader.load_from_options(**creds)
+sess = session.Session(auth=auth)
+# nova credentials
+nova = client.Client(2, session=sess)
+#client.flavors.list()
+print(nova.flavors.list())
