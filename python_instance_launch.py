6172e44acf4776c7ae386bc26520263d14b88a25
Xiang Li <xli@nevada.unr.edu>
2020-09-30 21:43:38 +0000

    Removed test code and changed parameters to variables for create

--- python_instance_launch.py
+++ python_instance_launch.py
@@ -11,9 +11,9 @@ from neutronclient.v2_0 import client as nt_client
 # Define variables
 flavor_name = 'm1.nano'
 key_name = 'demotestkey1'
-network_name = 'selfservice'
-sec_group = 'default'
-
+sec_group = ['default'] # Must be a list
+network_id = [{'net-id': '638ae64c-53af-41f1-bda6-4ef5430f4b12' }] # Must be a dict
+instance_name = 'py_test_1'
 # Define image IDs
 cirros_image_ID = '8ecbbd50-86a0-4948-9a38-e7d978b8e3d3'
 win10_image_ID = '35b5f896-54b7-429f-b3d6-346c10898f58'
@@ -32,11 +32,11 @@ def get_keystone_cred():
     #d['tenant_name'] = os.environ['OS_TENANT_NAME']
     return d
 
-def get_nova_cred():
-    d = {}
-    d['username'] = os.environ['OS_USERNAME']
-    d['api-key'] = os.environ['OS_PASSWORD']
-print "Hello"
+#def get_nova_cred():
+#    d = {}
+#    d['username'] = os.environ['OS_USERNAME']
+#    d['api-key'] = os.environ['OS_PASSWORD']
+#print "Hello"
 
 
 if __name__ == "__main__":
@@ -55,8 +55,6 @@ if __name__ == "__main__":
     neutron = nt_client.Client(session=sess)
     #print neutron.list_networks(name='selfservice')
 
-    #client.flavors.list()
-    #print(nova.flavors.list())
     # Launching instances
     # Set all parameters for instance launching
     # Set flavor
@@ -71,14 +69,6 @@ if __name__ == "__main__":
     except:
         print("The image could not be found.")
         sys.exit(1)
-    # Set network
-    try:
-        net_obj = neutron.list_networks(name=network_name)
-    except:
-        print("The network could not be found.")
-        sys.exit(1)
-        
-  #  print(fl_obj)
- #   print(im_obj)
-    nova.servers.create("py_test_Cir", im_obj, fl_obj, security_groups=['default'], key_name=key_name, nics=[{'net-id':'638ae64c-53af-41f1-bda6-4ef5430f4b12'}])
-
+    # Launch the instance using the nova create function
+    nova.servers.create(instance_name, im_obj, fl_obj, security_groups=sec_group, key_name=key_name, nics=network_id)
+print "done"
