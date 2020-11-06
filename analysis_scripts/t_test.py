e9001a6ebf3b964b631e0587d236e5773ca9b9be
xliUNR <xli@nevada.unr.edu>
2020-11-05 19:46:50 -0800

    fixed command line argument parsing for t_test

--- analysis_scripts/t_test.py
+++ analysis_scripts/t_test.py
@@ -2,6 +2,7 @@
 
 
 #imports
+import sys
 import pandas as pd
 import argparse
 
@@ -10,7 +11,11 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(file1, help="file name for first data file")
-    parser.add_argument(file2, help="file name for second data file")
+    parser.add_argument("file1", help="file name for first data file")
+    parser.add_argument("file2", help="file name for second data file")
+    args =  parser.parse_args()
+    #print(args.file1, args.file2 )
+    # Import data into dataframes
+    data1 = pd.read_csv(args.file1)
+    data2 = pd.read_csv(args.file2)
 
-    print(parser.file1, parser.file2)
