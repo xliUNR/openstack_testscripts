54901820e98f61a116eed65cf37058650ee374cb
xliUNR <xli@nevada.unr.edu>
2020-11-08 20:07:37 -0800

    added scipy and dataframe creation from file

--- analysis_scripts/t_test.py
+++ analysis_scripts/t_test.py
@@ -5,17 +5,22 @@
 import sys
 import pandas as pd
 import argparse
-
+from scipy import stats
 
 #read in data from text
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file1", help="file name for first data file")
-    parser.add_argument("file2", help="file name for second data file")
+#   parser.add_argument("file2", help="file name for second data file")
+    parser.add_argument("col_sep", help = "Which column would you like to seperate the data")
+    parser,add_argument("row_sep", help = "Row value to seperate data")
     args =  parser.parse_args()
     #print(args.file1, args.file2 )
     # Import data into dataframes
-    data1 = pd.read_csv(args.file1)
-    data2 = pd.read_csv(args.file2)
+    df1 = pd.read_csv(args.file1)
+#    data2 = pd.read_csv(args.file2)
+    data1 = df1[df1.col_sep == row_sep]
+    data2 = df1[df1.col_sep != row_sep]
 
+    
