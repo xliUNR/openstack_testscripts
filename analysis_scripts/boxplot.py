d8eb4116b291f43e3a7a179408e03653db89b7b2
xliUNR <xli@nevada.unr.edu>
2020-11-19 22:41:58 -0800

    Made script to print a 2 element box plot. Need to add labels for statistics

---
+++ analysis_scripts/boxplot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+#imports
+import pandas as pd
+import argparse
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    # parsing stuff
+    parser = argparse.ArgumentParser()
+    parser.add_argument("in_file", help="Input file path")
+    parser.add_argument("col_sep", help="Column index to seperate data")
+    parser.add_argument("row_sep", help="Row value to seperate data, typicallly windows or cirros")
+    parser.add_argument("t_col", type=int, help="Integer specifying which column to graph")
+    parser.add_argument("plot_title", help="title of plot")
+    args = parser.parse_args()
+    col_sep = args.col_sep
+    # read in data
+    args = parser.parse_args()
+    col_sep = args.col_sep
+    row_sep = args.row_sep
+    t_col = args.t_col
+    #print(args.file1, args.file2 )
+    # Import data into dataframes
+    df1 = pd.read_excel(args.in_file)
+#    data2 = pd.read_csv(args.file2)
+    data1 = df1[df1[args.col_sep] == args.row_sep]
+    data2 = df1[df1[args.col_sep] != args.row_sep]
+    data1 = data1.iloc[:,args.t_col]
+    data2 = data2.iloc[:,args.t_col]
+    merged_data = [data1, data2]
+#   print(merged_data)
+    fig1, ax1 = plt.subplots()
+    ax1.set_title(args.plot_title)
+    plt.ylabel('Time (s)')
+#   plt.xlabel(['Cirros','Windows'])
+    ax1.boxplot(merged_data, labels=['CirrOS','Windows'])
+    plt.show()
+
